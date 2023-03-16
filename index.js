6af3ba3c6a666f016425efd1ff566d2127456a61
Zakatsiura Ihor <120773721+Zakatsiura@users.noreply.github.com>
2023-03-16 01:38:47 +0000

    add changes for mobile

--- index.js
+++ index.js
@@ -5,16 +5,31 @@ const prevs = document.getElementsByClassName('prev')[0];
 const nexts = document.getElementsByClassName('next')[0];
 let num = 1;
 
+const elem = document.querySelector('.slider');
+let width = parseFloat(window.getComputedStyle(elem).getPropertyValue('width'));
+const mediaQuery = window.matchMedia('(max-width: 768px)');
+
+function onWindowResize() {
+    width = parseFloat(window.getComputedStyle(elem).getPropertyValue('width'));
+    if (mediaQuery.matches) {
+        width *= 2;
+    }
+    boxList.style.transition = '0s';
+    boxList.style.left = (num * -width) / 2 + 'px';
+}
+
+window.addEventListener('resize', onWindowResize);
+
 nexts.onmousedown = function () {
     if (num === lengh - 2) {
         boxList.style.transition = '0s';
         num = 0;
-        boxList.style.left = num * -500 + 'px';
+        boxList.style.left = (num * -width) / 2 + 'px';
     }
 };
 nexts.onmouseup = function () {
     num++;
-    boxList.style.left = num * -500 + 'px';
+    boxList.style.left = (num * -width) / 2 + 'px';
     boxList.style.transition = '0.5s';
 };
 
@@ -22,11 +37,11 @@ prevs.onmousedown = function () {
     if (num === 0) {
         boxList.style.transition = '0s';
         num = lengh - 2;
-        boxList.style.left = num * -500 + 'px';
+        boxList.style.left = (num * -width) / 2 + 'px';
     }
 };
 prevs.onmouseup = function () {
     num--;
-    boxList.style.left = num * -500 + 'px';
+    boxList.style.left = (num * -width) / 2 + 'px';
     boxList.style.transition = '0.5s';
 };
