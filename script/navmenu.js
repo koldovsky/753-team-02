c1020050d4b70290b513d97f89968ba62bc303ad
auggera <114820190+auggera@users.noreply.github.com>
2023-03-17 01:29:48 +0000

    working on mobile navbar

--- script/navmenu.js
+++ script/navmenu.js
@@ -1,13 +1,12 @@
+//Augustin
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
-document.querySelectorAll(".nav-link").forEach(n => n.
-    addEventListener("click" () => {
+document.querySelectorAll(".nav-menu-link").forEach(n => n.
+    addEventListener("click", () => {
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
-        
     }))
