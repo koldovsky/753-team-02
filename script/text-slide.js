121dda49fd2d0031f793854b1aa613ff39e1027a
Ruslan Kozhakin <125094408+Ruslan-KozhakIn@users.noreply.github.com>
2023-04-06 13:26:52 +0000

    formatted js text slide code

--- script/text-slide.js
+++ script/text-slide.js
@@ -1,57 +1,46 @@
-const stepsSliderTop = document.getElementById('stepsSliderTop');
-const textSliderTop = document.getElementById('textSliderTop');
+const stepsSliderTop = document.getElementById("stepsSliderTop");
+const textSliderTop = document.getElementById("textSliderTop");
 
-stepsSliderTop.addEventListener('click', function() {
-if (textSliderTop.style.display === 'none') {
-   textSliderTop.style.display = 'block';
-}   else {
-   textSliderTop.style.display = 'none';
-}
-
-})
-
-
-const stepsSliderMiddle = document.getElementById('stepsSliderMiddle');
-const textSliderMiddle = document.getElementById('textSliderMiddle');
-
-stepsSliderMiddle.addEventListener('click', function(){
-
-if (textSliderMiddle.style.display === 'none') {
-   textSliderMiddle.style.display = 'block';
- } else {
-   textSliderMiddle.style.display = 'none'
- }
-
-})
-
-
-const stepsSliderBottom = document.getElementById('stepsSliderBottom');
-const textSliderBottom = document.getElementById('textSliderBottom');
-
-stepsSliderBottom.addEventListener('click', function(){
-
-if (textSliderBottom.style.display === 'none') {
-   textSliderBottom.style.display = 'block';
- } else {
-   textSliderBottom.style.display = 'none'
- }
-
-})
-
-const stepsIcon = document.querySelectorAll('.steps-icon');
-
-
-stepsIcon.forEach(stepsIcon => {
-   let isButtonOn = true;
-stepsIcon.addEventListener('click', () => {
-isButtonOn = !isButtonOn;
-stepsIcon.classList.toggle('slider-bottom-on', isButtonOn);
-
-
-})
-
+stepsSliderTop.addEventListener("click", function () {
+  if (textSliderTop.style.display === "none") {
+    textSliderTop.style.display = "block";
+  } else {
+    textSliderTop.style.display = "none";
+  }
 });
-   
+
+const stepsSliderMiddle = document.getElementById("stepsSliderMiddle");
+const textSliderMiddle = document.getElementById("textSliderMiddle");
+
+stepsSliderMiddle.addEventListener("click", function () {
+  if (textSliderMiddle.style.display === "none") {
+    textSliderMiddle.style.display = "block";
+  } else {
+    textSliderMiddle.style.display = "none";
+  }
+});
+
+const stepsSliderBottom = document.getElementById("stepsSliderBottom");
+const textSliderBottom = document.getElementById("textSliderBottom");
+
+stepsSliderBottom.addEventListener("click", function () {
+  if (textSliderBottom.style.display === "none") {
+    textSliderBottom.style.display = "block";
+  } else {
+    textSliderBottom.style.display = "none";
+  }
+});
+
+const stepsIcon = document.querySelectorAll(".steps-icon");
+
+stepsIcon.forEach((stepsIcon) => {
+  let isButtonOn = true;
+  stepsIcon.addEventListener("click", () => {
+    isButtonOn = !isButtonOn;
+    stepsIcon.classList.toggle("slider-bottom-on", isButtonOn);
+  });
+});
+
   
 
    
